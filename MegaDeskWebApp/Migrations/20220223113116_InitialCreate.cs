bed0ae784283c04d5d3f4eef8774c85af2e703e0
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-02-23 15:25:32 +0200

    initial commit

---
+++ MegaDeskWebApp/Migrations/20220223113116_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDeskWebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    DeskWidth = table.Column<int>(type: "int", nullable: false),
+                    DeskDepth = table.Column<int>(type: "int", nullable: false),
+                    NumberOfDrawers = table.Column<int>(type: "int", nullable: false),
+                    DesktopMaterial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RushOrder = table.Column<int>(type: "int", nullable: false),
+                    DeskQuoteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeskQuotePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+        }
+    }
+}
---
+++ MegaDeskWebApp/Models/DeskQuote.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MegaDeskWebApp.Models
+{
+    public class DeskQuote
+    {
+        public int ID { get; set; }
+
+        [StringLength(70, MinimumLength = 3)]
+        [Required]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Range(24, 96)]
+        [Required]
+        public int DeskWidth { get; set; }
+
+        [Range(12, 48)]
+        [Required]
+        public int DeskDepth { get; set; }
+
+        [Range(1, 7)]
+        [Required]
+        public int NumberOfDrawers { get; set; }
+
+        [Required]
+        public string DesktopMaterial { get; set; }
+
+        [Required]
+        public int RushOrder { get; set; }
+
+        [Display(Name = "DeskQuote Date")]
+        [DataType(DataType.Date)]
+        public DateTime DeskQuoteDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal DeskQuotePrice { get; set; }
+    }
+    
+}
