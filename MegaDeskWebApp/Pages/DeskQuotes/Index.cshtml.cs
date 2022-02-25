9e761b89b0e3eb71b2ab8c6fecfa4217fa85d0e7
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-02-25 12:12:47 +0200

    Added search and made some input fields hidden

--- MegaDeskWebApp/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDeskWebApp/Pages/DeskQuotes/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskWebApp.Data;
 using MegaDeskWebApp.Models;
@@ -23,7 +24,8 @@ namespace MegaDeskWebApp.Pages.DeskQuotes
         public string DateSort { get; set; }
         //public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
         public IList<DeskQuote> DeskQuote { get;set; }
 
         public async Task OnGetAsync(string sortOrder)
@@ -34,7 +36,7 @@ namespace MegaDeskWebApp.Pages.DeskQuotes
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
 
-            IQueryable<DeskQuote> descQuotes = from dq in _context.DeskQuote
+            IQueryable<DeskQuote> deskQuotes = from dq in _context.DeskQuote
                                                select dq;
 
 
@@ -42,20 +44,25 @@ namespace MegaDeskWebApp.Pages.DeskQuotes
 
             {
                 case "name_desc":
-                    descQuotes = descQuotes.OrderByDescending(dq => dq.CustomerName);
+                    deskQuotes = deskQuotes.OrderByDescending(dq => dq.CustomerName);
                     break;
                 case "Date":
-                    descQuotes = descQuotes.OrderBy(dq => dq.DeskQuoteDate);
+                    deskQuotes = deskQuotes.OrderBy(dq => dq.DeskQuoteDate);
                     break;
                 case "date_desc":
-                    descQuotes = descQuotes.OrderByDescending(dq => dq.DeskQuoteDate);
+                    deskQuotes = deskQuotes.OrderByDescending(dq => dq.DeskQuoteDate);
                     break;
                 default:
-                    descQuotes = descQuotes.OrderBy(dq => dq.CustomerName);
+                    deskQuotes = deskQuotes.OrderBy(dq => dq.CustomerName);
                     break;
             }
 
-            DeskQuote = await descQuotes.AsNoTracking().ToListAsync();
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                deskQuotes = deskQuotes.Where(dq => dq.CustomerName.Contains(SearchString));
+            }
+
+            DeskQuote = await deskQuotes.AsNoTracking().ToListAsync();
         }
     }
 }
